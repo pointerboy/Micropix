4de8d605f1bb74111b3af8ed9868288342de9165
pointerboy <lukatrbovic@outlook.com>
2019-02-10 20:01:46 +0100

    blocked resizing

--- Form.Designer.cs
+++ Form.Designer.cs
@@ -31,14 +31,14 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form));
             this.snipBtn = new System.Windows.Forms.Button();
             this.statusStrip1 = new System.Windows.Forms.StatusStrip();
-            this.notificationLabel = new System.Windows.Forms.ToolStripStatusLabel();
-            this.autosaveCheckBox = new System.Windows.Forms.CheckBox();
-            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
-            this.timerCombobox = new System.Windows.Forms.ComboBox();
             this.toolStripDropDownButton1 = new System.Windows.Forms.ToolStripDropDownButton();
             this.styleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.freeformToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.fullScreenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.notificationLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.autosaveCheckBox = new System.Windows.Forms.CheckBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.timerCombobox = new System.Windows.Forms.ComboBox();
             this.statusStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -49,7 +49,7 @@
             this.snipBtn.Size = new System.Drawing.Size(57, 45);
             this.snipBtn.TabIndex = 0;
             this.snipBtn.UseVisualStyleBackColor = true;
-            this.snipBtn.Click += new System.EventHandler(this.snipBtn_Click);
+            this.snipBtn.Click += new System.EventHandler(this.snipBtn_ClickAsync);
             // 
             // statusStrip1
             // 
@@ -61,6 +61,41 @@
             this.statusStrip1.Size = new System.Drawing.Size(311, 22);
             this.statusStrip1.TabIndex = 1;
             this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripDropDownButton1
+            // 
+            this.toolStripDropDownButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripDropDownButton1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.styleToolStripMenuItem});
+            this.toolStripDropDownButton1.Image = global::Micropix.Properties.Resources.favicon1;
+            this.toolStripDropDownButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripDropDownButton1.Name = "toolStripDropDownButton1";
+            this.toolStripDropDownButton1.Size = new System.Drawing.Size(29, 20);
+            this.toolStripDropDownButton1.Text = "toolStripDropDownButton1";
+            this.toolStripDropDownButton1.Click += new System.EventHandler(this.toolStripDropDownButton1_Click);
+            // 
+            // styleToolStripMenuItem
+            // 
+            this.styleToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.freeformToolStripMenuItem,
+            this.fullScreenToolStripMenuItem});
+            this.styleToolStripMenuItem.Name = "styleToolStripMenuItem";
+            this.styleToolStripMenuItem.Size = new System.Drawing.Size(116, 22);
+            this.styleToolStripMenuItem.Text = "Options";
+            // 
+            // freeformToolStripMenuItem
+            // 
+            this.freeformToolStripMenuItem.Name = "freeformToolStripMenuItem";
+            this.freeformToolStripMenuItem.Size = new System.Drawing.Size(133, 22);
+            this.freeformToolStripMenuItem.Text = "Freeform";
+            this.freeformToolStripMenuItem.Click += new System.EventHandler(this.freeformToolStripMenuItem_Click);
+            // 
+            // fullScreenToolStripMenuItem
+            // 
+            this.fullScreenToolStripMenuItem.Name = "fullScreenToolStripMenuItem";
+            this.fullScreenToolStripMenuItem.Size = new System.Drawing.Size(133, 22);
+            this.fullScreenToolStripMenuItem.Text = "Full screen ";
+            this.fullScreenToolStripMenuItem.Click += new System.EventHandler(this.fullScreenToolStripMenuItem_Click);
             // 
             // notificationLabel
             // 
@@ -92,41 +127,6 @@
             this.timerCombobox.Size = new System.Drawing.Size(121, 21);
             this.timerCombobox.TabIndex = 3;
             // 
-            // toolStripDropDownButton1
-            // 
-            this.toolStripDropDownButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-            this.toolStripDropDownButton1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.styleToolStripMenuItem});
-            this.toolStripDropDownButton1.Image = global::Micropix.Properties.Resources.favicon1;
-            this.toolStripDropDownButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.toolStripDropDownButton1.Name = "toolStripDropDownButton1";
-            this.toolStripDropDownButton1.Size = new System.Drawing.Size(29, 20);
-            this.toolStripDropDownButton1.Text = "toolStripDropDownButton1";
-            this.toolStripDropDownButton1.Click += new System.EventHandler(this.toolStripDropDownButton1_Click);
-            // 
-            // styleToolStripMenuItem
-            // 
-            this.styleToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.freeformToolStripMenuItem,
-            this.fullScreenToolStripMenuItem});
-            this.styleToolStripMenuItem.Name = "styleToolStripMenuItem";
-            this.styleToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
-            this.styleToolStripMenuItem.Text = "Options";
-            // 
-            // freeformToolStripMenuItem
-            // 
-            this.freeformToolStripMenuItem.Name = "freeformToolStripMenuItem";
-            this.freeformToolStripMenuItem.Size = new System.Drawing.Size(133, 22);
-            this.freeformToolStripMenuItem.Text = "Freeform";
-            this.freeformToolStripMenuItem.Click += new System.EventHandler(this.freeformToolStripMenuItem_Click);
-            // 
-            // fullScreenToolStripMenuItem
-            // 
-            this.fullScreenToolStripMenuItem.Name = "fullScreenToolStripMenuItem";
-            this.fullScreenToolStripMenuItem.Size = new System.Drawing.Size(133, 22);
-            this.fullScreenToolStripMenuItem.Text = "Full screen ";
-            this.fullScreenToolStripMenuItem.Click += new System.EventHandler(this.fullScreenToolStripMenuItem_Click);
-            // 
             // Form
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -136,7 +136,9 @@
             this.Controls.Add(this.autosaveCheckBox);
             this.Controls.Add(this.statusStrip1);
             this.Controls.Add(this.snipBtn);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
             this.Name = "Form";
             this.Text = "Micropix";
             this.Load += new System.EventHandler(this.Form_Load);
